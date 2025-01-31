17e5fc1802f5318f4aadaf5fd81f52961c9640b1
Saulo Pereira <saulopsg@hotmail.com>
2025-01-31 14:49:53 -0300

    Atualiza a lista na tela

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Array para armazenar os amigos
+let amigos = [];
+
+// Função para adicionar amigo à lista
+function adicionarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nomeAmigo = inputAmigo.value.trim();
+
+    if (nomeAmigo === '') {
+        // Se nome não foi inserido, exiba o alert
+        alert('Por favor insira o nome do amigo secreto.');
+        return;
+    }
+
+    if (!amigos.includes(nomeAmigo)) {
+        amigos.push(nomeAmigo);
+        atualizarLista();
+        inputAmigo.value = ''; // Limpa o campo de texto
+    } else {
+        alert('Amigo já adicionado.');
+    }
+}
